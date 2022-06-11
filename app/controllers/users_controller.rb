43cc2a069b77ffb6143f9e07aa72ac93d63152a4
Jonathan Sivahera <jsivahera@icloud.com>
2022-06-11 06:18:55 +0200

    Refactor to use best practices

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
   def index; end
 
-  def show_user; end
-
-  def show_user_posts; end
-
-  def show_user_post; end
+  def show; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get "users", to: "users#index"
-  get "users/:user_id/posts/:post_id", to: "users#show_user_post"
-  get "users/:user_id/posts", to: "users#show_user_posts"
-  get "users/:user_id", to: "users#show_user"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
