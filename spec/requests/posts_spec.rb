009c8de5670ece1ee4f52255176bb2b28cc0b322
Jonathan Sivahera <jsivahera@icloud.com>
2022-06-11 09:19:28 +0200

    Improve tests architecture

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,17 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  it "tests the #index action" do
-    get "/users/12/posts"
-    expect(response).to render_template(:index)
-    expect(response.body).to include("Here is a list of posts for a given user")
-    expect(response).to have_http_status(:ok)
+  describe "tests the #index action" do
+    before(:each) do
+      get "/users/12/posts"
+    end
+
+    it "should render the correct template" do
+      expect(response).to render_template(:index)
+    end
+
+    it "should have the correct placeholder text" do
+      expect(response.body).to include("Here is a list of posts for a given user")
+    end
+
+    it "should have a correct response status" do
+      expect(response).to have_http_status(:ok)
+    end
   end
 
-  it "tests the #show action" do
-    get "/users/12/posts/11"
-    expect(response).to render_template(:show)
-    expect(response.body).to include("Here is a single post made by a given user")
-    expect(response).to have_http_status(:ok)
+  describe "tests the #show action" do
+    before(:each) do
+      get "/users/12/posts/11"
+    end
+
+    it "should render the correct template" do
+      expect(response).to render_template(:show)
+    end
+
+    it "should have the correct placeholder text" do
+      expect(response.body).to include("Here is a single post made by a given user")
+    end
+
+    it "should have a correct response status" do
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  it "tests the #index action" do
-    get "/users"
-    expect(response).to render_template(:index)
-    expect(response.body).to include("Here is a list of all the users")
-    expect(response).to have_http_status(:ok)
+  describe "tests the #index action" do
+    before(:each) do
+      get "/users"
+    end
+
+    it "should render the correct template" do
+      expect(response).to render_template(:index)
+    end
+
+    it "should have the correct placeholder text" do
+      expect(response.body).to include("Here is a list of all the users")
+    end
+
+    it "should have a correct response status" do
+      expect(response).to have_http_status(:ok)
+    end
   end
 
-  it "tests the #show action" do
-    get "/users/22"
-    expect(response).to render_template(:show)
-    expect(response.body).to include("Here is a single given user page")
-    expect(response).to have_http_status(:ok)
+  describe "tests the #show action" do
+    before(:each) do
+      get "/users/22"
+    end
+
+    it "should render the correct template" do
+      expect(response).to render_template(:show)
+    end
+
+    it "should have the correct placeholder text" do
+      expect(response.body).to include("Here is a single given user page")
+    end
+
+    it "should have a correct response status" do
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
