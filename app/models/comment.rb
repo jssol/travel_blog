b93c8f24e004cbdad9a3a8230e979db2c34e41bd
Jonathan Sivahera <jsivahera@icloud.com>
2022-06-17 17:41:48 +0200

    Setup associations for Like, Post and Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :user, foreign_key: "author_id"
+  belongs_to :user, class_name: "user", foreign_key: "author_id"
+  belongs_to :post, class_name: "post", foreign_key: "post_id"
 end
