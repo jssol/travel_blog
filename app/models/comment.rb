fe6265b1e792683edb57a772ed2ae1223f052984
Jonathan Sivahera <jsivahera@icloud.com>
2022-06-18 01:46:19 +0200

    Implement required changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
-  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post
 
   def self.update_post_comments_count(post)
     post_comments_count = Comment.where(post_id: post.id).count
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
-  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post
 
   def self.update_post_likes_count(post)
     post_likes_count = Like.where(post_id: post.id).count
