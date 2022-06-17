a011ef8ad188fe3c95c2a1d4c54b28cd875bc546
Jonathan Sivahera <jsivahera@icloud.com>
2022-06-18 00:03:55 +0200

    Run new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_17_210104) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_17_215430) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_17_210104) do
     t.datetime "updated_at", null: false
     t.string "address"
     t.index ["address"], name: "index_users_on_address"
+    t.index ["id"], name: "index_users_on_id", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end
