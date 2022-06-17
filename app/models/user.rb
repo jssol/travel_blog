be715a2495bb35b12feeb0beceb5f880049133df
Jonathan Sivahera <jsivahera@icloud.com>
2022-06-17 20:44:24 +0200

    Fix class_name in Models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :comments, class_name: 'comment', foreign_key: 'author_id'
-  has_many :likes, class_name: 'like', foreign_key: 'author_id'
-  has_many :posts, class_name: 'post', foreign_key: 'author_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'author_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'author_id'
+  has_many :posts, class_name: 'Post', foreign_key: 'author_id'
 end
