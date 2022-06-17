563f10ce3e8403e5f219147bdfa2a7ce5e487387
Jonathan Sivahera <jsivahera@icloud.com>
2022-06-17 17:49:49 +0200

    Fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-  has_many :comments, class_name: "comment", foreign_key: "post_id"
-  belongs_to :user, class_name: "user", foreign_key: "author_id"
+  has_many :comments, class_name: 'comment', foreign_key: 'post_id'
+  belongs_to :user, class_name: 'user', foreign_key: 'author_id'
 end
