a2e0e11cff42187c03b6f096333d032c6b3ebedb
Jonathan Sivahera <jsivahera@icloud.com>
2022-06-18 23:24:34 +0200

    Add comments form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,42 @@
 class CommentsController < ApplicationController
-  def new; end
+  def new
+    @comment = Comment.new
+    @user = current_user
+    @post = Post.first
+    respond_to do |format|
+      format.html { render :new, locals: { comment: @comment } }
+    end
+  end
 
-  def create; end
+  def create
+    # new object from params
+    @comment = Comment.new(post_params)
+
+    # respond_to block
+    respond_to do |format|
+      format.html do
+        if @comment.save
+          # success message
+          post = Post.find(post_params[:author_id])
+          Comment.update_post_comments_count(post)
+          flash[:success] = 'Comment created successfully'
+          # redirect to index
+          redirect_to user_posts_url
+        else
+          flash.now[:error] = 'Error: Comment could not be created'
+          # render new
+          render :new, locals: { comment: @comment }
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    post_params = params.require(:comment).permit(:text)
+    post_params[:author_id] = current_user.id
+    post_params[:post_id] = Post.first.id
+    post_params
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  after_save :update_post_comments_count
-
-  def update_post_comments_count
-    post.increment!(:comments_counter)
+  def self.update_post_comments_count(post)
+    post_comments_count = Post.where(author_id: post.author_id).count
+    post.comments_counter = post_comments_count
   end
 end
